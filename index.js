639039f34454de34b73ad7f181b9522515411fb7
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-20 22:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solutions in this file
 //[ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ]
-//[ 'Lisa', 'Kaitlin', 'Jan' ]
+// [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+const =
 
