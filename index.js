281017b8a52afaf9fe496718efa1916cb2841b6c
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-20 22:33:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Code your solutions in this file
 //[ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ]
-// [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+//names = [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
 
-const =
+function writeCards(names) {
+  for let = i; i < names.length; i++;
+  `Thank you, ${}, for the wonderful surprise gift!`
+  
+}
 
