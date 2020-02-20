2254da50f68d64364f3ec306677db5fe315ef4c3
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-20 22:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Code your solutions in this file
-[ 'Lisa', 'Kaitlin', 'Jan' ]
+//[ 'Thank you, Lisa, for the wonderful surprise gift!', 'Thank you, Kaitlin, for the wonderful surprise gift!', 'Thank you, Jan, for the wonderful surprise gift!' ]
+//[ 'Lisa', 'Kaitlin', 'Jan' ]
 
